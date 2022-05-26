00f777c644096ad04ef6033c6ea5c58b3f59c29e
jesi318 <jeswin.p.jaison@gmail.com>
2022-05-26 12:44:55 +0530

    commit

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from crypt import methods
+
 from app import app
 from flask import render_template
 import forms
@@ -8,9 +8,13 @@ import forms
 def index():
     return render_template('index.html', current_title="New Index Title")
 
+@app.route('/')
+
 @app.route('/about' , methods=['GET', 'POST'])
 def about():
     form = forms.AddTaskForm()
+    if form.validate_on_submit():
+        return render_template('about.html', form=form, title = form.title.data)
 
     return render_template ('about.html', current_title="About page title", form=form)
 
